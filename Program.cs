a840c3d2a44e09008b8e978c7d5f023fbb028a75
cobolaz <109870484+cobolaz@users.noreply.github.com>
2026-02-14 16:01:13 -0700

    Implement global exception handler
    Added global exception handler to return JSON error response.

--- Program.cs
+++ Program.cs
@@ -163,6 +163,19 @@ namespace UserManagementAPI.Controllers
     }
 }
 
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/json";
+
+        await context.Response.WriteAsJsonAsync(new
+        {
+            error = "An unexpected error occurred. Please try again later."
+        });
+    });
+});
 
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
